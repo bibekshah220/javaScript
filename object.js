5f42e5fa572f21529a5c39a2a1e09d4570fb6164
bibekshah220 <bibekshah425@gmail.com>
2026-02-06 22:49:43 +0545

    object

---
+++ object.js
@@ -0,0 +1,21 @@
+const laptop = {
+    price:99999,
+    model:"MacBook",
+    colour: "black"
+}
+// console.log(laptop)
+
+const mobile = {
+    model:"iphone 19",
+    proce: 100000,
+    colour: "black"
+}
+// console.log(mobile)
+
+const car = {
+    model:"BMW",
+    price:9999999,
+    colour:"bblack"
+}
+console.log(car.model)
+console.log(car.model)
